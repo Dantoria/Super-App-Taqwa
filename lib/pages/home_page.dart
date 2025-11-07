e1f36b7ca3e96fecc26df82221b7f812c3dc8ace
Dantoria <zaidan.meiluano@gmail.com>
2025-11-07 09:15:09 +0700

    Belajar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -14,13 +14,13 @@ class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState(
-    int _currentIndex = 0;
-  );
+  State<HomePage> createState() => _HomePageState();
 }
+
 
 class _HomePageState extends State<HomePage> {
   final CarouselController _controller = CarouselController();
+  int _currentIndex = 0;
 
   final PosterList = const <String> [
     'assets/images/ramadhan-kareem.png'
@@ -148,11 +148,50 @@ class _HomePageState extends State<HomePage> {
           itemBuilder: (context, index, realIndex) {
             final poster = PosterList[index];
             return Container(
-              child: Image.asset(poster),
+              margin: EdgeInsets.all(15),
+              child: ClipRRect(
+                borderRadius: BorderRadiusGeometry.circular(20),
+                child: Image.asset(
+                  poster,
+                  width: double.infinity,
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
             );
         },
-          options: options)
+          options: CarouselOptions(
+            autoPlay: true,
+            height: 300,
+            enlargeCenterPage: true,
+            viewportFraction: 0.7,
+            onPageChanged: (index, reason) {
+              setState(()=> _currentIndex = index);
+            }
+          )),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: PosterList.asMap().entries.map((entry){
+              return GestureDetector(
+                onTap: () => _currentIndex.animatetoPage(entry.key),
+                child: Container(
+                  width: 10,
+                  height: 10,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: _currentIndex == entry.key
+                        ? Colors.amber
+                        : Colors.grey
+                  ),
+                ),
+              );
+            }).toList()
+          )
       ],
     );
   }
+}
+
+extension on int {
+  void animatetoPage(int key) {}
 }
