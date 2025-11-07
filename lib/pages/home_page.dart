e782c30dee7693e2d1d77eb66f242e6dec53922b
Dantoria <zaidan.meiluano@gmail.com>
2025-11-07 09:54:46 +0700

    belajar menu item

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -34,107 +34,37 @@ class _HomePageState extends State<HomePage> {
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                width: double.infinity,
-                height: 130,
-                decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 47, 141, 133),
-                  borderRadius: BorderRadius.circular(15)
-                ),
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: <Widget>[
-                      InkWell(
-                        onTap: () {},
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                            decoration: BoxDecoration(),
-                            child: Column(
-                              children: [
-                                Image.asset('assets/images/ic_menu_doa.png'),
-                                Text(
-                                  'Doa & Zikir',
-                                style:  TextStyle(
-                                  fontFamily: 'poppinsRegular',
-                                  color: Colors.white,
-                                ),)
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                        InkWell(
-                        onTap: () {},
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                            decoration: BoxDecoration(),
-                            child: Column(
-                              children: [
-                                Image.asset('assets/images/ic_menu_jadwal_sholat.png'),
-                                Text(
-                                  'Jadwal Sholat',
-                                style:  TextStyle(
-                                  fontFamily: 'poppinsRegular',
-                                  color: Colors.white,
-                                ),)
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                        InkWell(
-                        onTap: () {},
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                            decoration: BoxDecoration(),
-                            child: Column(
-                              children: [
-                                Image.asset('assets/images/ic_menu_video_kajian.png'),
-                                Text(
-                                  'Video kajian',
-                                style:  TextStyle(
-                                  fontFamily: 'poppinsRegular',
-                                  color: Colors.white,
-                                ),)
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                        InkWell(
-                        onTap: () {},
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                            decoration: BoxDecoration(),
-                            child: Column(
-                              children: [
-                                Image.asset('assets/images/ic_menu_zakat.png'),
-                                Text(
-                                  'zakat',
-                                style:  TextStyle(
-                                  fontFamily: 'poppinsRegular',
-                                  color: Colors.white,
-                                ),)
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            Column()
+            _buildCarouselSection(),
+            _buildCarouselSection()
           ],
         ),
+
+      ),
+    );
+  }
+  
+  Widget _buildMenuItem(String iconpath, String title, String routename){
+    return Column(
+      children: [
+        Image.asset(iconpath),
+        Text(title)
+      ],
+    );
+  }
+  
+  Widget _buildMenuGridSection(){
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: GridView.count(
+        crossAxisCount: 4,
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        children: [
+          _buildMenuItem(),
+          _buildMenuItem(),
+          _buildMenuItem(),
+          _buildMenuItem(),
+        ],
       ),
     );
   }
