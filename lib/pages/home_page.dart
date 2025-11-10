1d94d780a5f679aca4cb9dfeda8ba12153279b90
Dantoria <zaidan.meiluano@gmail.com>
2025-11-10 14:20:42 +0700

    membuat menu dashboard islami

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,14 @@
-import 'dart:async';
+import 'dart:async'; //timer coundown
 import 'package:flutter/material.dart';
-import 'package:carousel_slider/carousel_slider.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
-import 'package:geolocator/geolocator.dart';
-import 'package:intl/intl.dart';
-import 'package:geocoding/geocoding.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'package:string_scanner/string_scanner.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:carousel_slider/carousel_slider.dart'; //carousel slider
+import 'package:http/http.dart' as http; //ambil data API
+import 'dart:convert'; //decode JSON
+import 'package:geolocator/geolocator.dart'; //GPS
+import 'package:geocoding/geocoding.dart'; //konversi GPS
+import 'package:intl/intl.dart'; //Format Nummber
+import 'package:permission_handler/permission_handler.dart'; //Izin Handler
+import 'package:shared_preferences/shared_preferences.dart'; // cache lokal
+import 'package:string_similarity/string_similarity.dart'; // fuzzy match string
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -17,86 +17,198 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-
 class _HomePageState extends State<HomePage> {
   final CarouselController _controller = CarouselController();
   int _currentIndex = 0;
 
-  final PosterList = const <String> [
-    'assets/images/ramadhan-kareem.png'
-    'assets/images/ idl-fitr.png'
-    'assets/images/idl-adh.png'
+  final posterlist = const <String>[
+    'assets/images/ramadhan-kareem.png',
+    'assets/images/idl-fitr.png',
+    'assets/images/idl-adh.png',
+    'assets/images/bg_afternoon.png',
+    'assets/images/bg_morning.png',
+    'assets/images/bg_night.png',
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            _buildCarouselSection(),
-            _buildCarouselSection()
-          ],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              //=========================================
+              //[MENU WAKTU SHOLAT BY LOKASI]
+              //=========================================
+              _buildHeroSection(),
+        
+              // ========================================
+              //[MENU SECTION]
+              // ========================================
+              _buidMenuGridSection(),
+              // ========================================
+              //[CAROUSEL SECTION]
+              // ========================================
+              _buildCaroucelSection(),
+            ],
+          ),
         ),
+      ),
+    );
+  }
 
-      ),
-    );
-  }
-  
-  Widget _buildMenuItem(String iconpath, String title, String routename){
-    return InkWell(
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(12),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.05),
-              blurRadius: 4,
-              offset: const Offset(0, 2)
+  //=======================================
+  //[MENU HERO WIDGET]
+  //=======================================
+  Widget _buildHeroSection() {
+    return Stack(
+      children: [
+        Container(
+          width: double.infinity,
+          height: 290,
+          decoration: BoxDecoration(
+            color: Color(0xFFB3E5FC),
+            borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(20),
+              bottomLeft: Radius.circular(20)
+            ),
+            image: DecorationImage(
+              image: AssetImage(
+                'asset/image/bg_night.png'
+              ),
+              fit: BoxFit.cover,
             )
-          ]
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 20,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget> [
+                Text('Assalamu\'alaikum',
+                style: TextStyle(
+                  fontFamily: 'PoppinsRegular',
+                  color: Colors.black,
+                  fontSize: 16,
+                  ),
+                ),
+                Text('Ngargoyoso',
+                style: TextStyle(
+                  fontFamily: 'PoppinsSemiBold',
+                  fontSize: 22
+                  ),
+                ),
+                Text(DateFormat('HH:mm').format(DateTime.now(),),
+                style: TextStyle(
+                  fontFamily: 'PoppinsBold',
+                  fontSize: 20,
+                  height: 1.2,
+                  color: Colors.white
+                ),),
+              ],
+            ),
+          ),
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(iconpath, width: 35,),
-            const SizedBox(height: 6,),
-            Text(title, style: TextStyle(
-              fontFamily: 'PoppinsRegular',
-              fontSize: 13,
-            ),)
-          ],
+      ],
+    );
+  }
+
+  //=======================================
+  //[MENU GRID SECTION WIDGET]
+  //=======================================
+  Widget _buidMenuItem(String iconPath, String title, String roudname) {
+    return Material(
+      child: InkWell(
+        onTap: () {
+          Navigator.pushNamed(context, roudname);
+        },
+        borderRadius: BorderRadius.circular(15),
+        splashColor: Colors.amber.withOpacity(0.2),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(12),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.05),
+                blurRadius: 4,
+                offset: const Offset(0, 2),
+              ),
+            ],
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(iconPath, width: 35),
+              const SizedBox(height: 6),
+              Text(
+                title,
+                style: TextStyle(fontFamily: 'Poppins-Regular', fontSize: 13),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
-  
-  Widget _buildMenuGridSection(){
+
+  //=======================================
+  //[MENU GRID SECTION WIDGET]
+  //=======================================
+  Widget _buidMenuGridSection() {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: GridView.count(
         crossAxisCount: 4,
         shrinkWrap: true,
+        mainAxisSpacing: 16,
+        crossAxisSpacing: 16,
         physics: const NeverScrollableScrollPhysics(),
         children: [
-          _buildMenuItem(),
-          _buildMenuItem(),
-          _buildMenuItem(),
-          _buildMenuItem(),
+          _buidMenuItem(
+            'assets/images/ic_menu_doa.png', //icon path
+            'Doa', // title
+            'Doa',
+          ), //route name
+          _buidMenuItem(
+            'assets/images/ic_menu_jadwal_sholat.png', //icon path
+            'sholat', //title
+            'Sholat',
+          ), //route name
+          _buidMenuItem(
+            'assets/images/ic_menu_Zakat.png', //icon patch
+            'Zakat', //title
+            'Zakat',
+          ), //route name
+          _buidMenuItem(
+            'assets/images/ic_menu_doa.png', //icon path
+            'Doa', // title
+            'Doa',
+          ), //route name
+          _buidMenuItem(
+            'assets/images/ic_play_video_kajian.png', //icon path
+            'Kajian', //title
+            'Kajian Islam',
+          ), //route name
         ],
       ),
     );
   }
 
-  Widget _buildCarouselSection() {
+  //=======================================
+  //[CAROUSEL SECTION]
+  //=======================================
+  Widget _buildCaroucelSection() {
     return Column(
       children: [
-        const SizedBox(height: 20,),
+        const SizedBox(height: 20),
         CarouselSlider.builder(
-          itemCount: PosterList.length,
-          itemBuilder: (context, index, realIndex) {
-            final poster = PosterList[index];
+          itemCount: posterlist.length,
+          itemBuilder: (context, index, realindex) {
+            final poster = posterlist[index];
             return Container(
               margin: EdgeInsets.all(15),
               child: ClipRRect(
@@ -104,44 +216,47 @@ class _HomePageState extends State<HomePage> {
                 child: Image.asset(
                   poster,
                   width: double.infinity,
-                  fit: BoxFit.fitWidth,
+                  fit: BoxFit.cover,
                 ),
               ),
             );
-        },
+          },
           options: CarouselOptions(
             autoPlay: true,
-            height: 300,
+            height: 270,
             enlargeCenterPage: true,
             viewportFraction: 0.7,
             onPageChanged: (index, reason) {
-              setState(()=> _currentIndex = index);
-            }
-          )),
+              setState(() => _currentIndex = index);
+            },
+          ),
+        ),
 
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: PosterList.asMap().entries.map((entry){
-              return GestureDetector(
-                onTap: () => _currentIndex.animatetoPage(entry.key),
-                child: Container(
-                  width: 10,
-                  height: 10,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: _currentIndex == entry.key
-                        ? Colors.amber
-                        : Colors.grey
-                  ),
+        // DOT INDIKATOR carousel
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: posterlist.asMap().entries.map((entry) {
+            return GestureDetector(
+              onTap: () => _currentIndex.animateToPage(entry.key),
+              child: Container(
+                width: 10,
+                height: 10,
+                margin: EdgeInsets.all(2),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: _currentIndex == entry.key
+                      ? Colors.amber
+                      : Colors.grey[400],
                 ),
-              );
-            }).toList()
-          )
+              ),
+            );
+          }).toList(),
+        ),
       ],
     );
   }
 }
 
 extension on int {
-  void animatetoPage(int key) {}
+  void animateToPage(int key) {}
 }
