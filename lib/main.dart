8b18e55cda650f19c47224abbbb2baf0cf4343ec
Dantoria <zaidan.meiluano@gmail.com>
2025-11-17 11:07:39 +0700

    Add Homepage + Zakatpage

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,23 @@
-import 'package:bitaqwa/pages/home_page.dart';
-import 'package:flutter/material.dart';
-
-void main() {
+void main() async {
+  /// 🔹 Pastikan Flutter sudah siap sebelum menjalankan async
   WidgetsFlutterBinding.ensureInitialized();
+ 
+  /// 🔹 Inisialisasi format tanggal bahasa Indonesia
+  /// Contoh hasil: “Senin, 6 November 2025”
+  await initializeDateFormatting('id_ID', null);
+ 
+  /// 🔹 Override SSL supaya koneksi HTTP/HTTPS yang self-signed tetap bisa jalan
+  HttpOverrides.global = MyHttpOverrides();
+ 
+  /// 🔹 Jalankan aplikasi utama
   runApp(const MyApp());
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+ 
+class MyHttpOverrides extends HttpOverrides {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      debugShowCheckedModeBanner: false,
-      routes: {
-        '/': (context) => HomePage()
-      },
-    );
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (cert, host, port) => true;
   }
 }
+A
